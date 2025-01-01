232e3d93c7498ce86e707496934372a459bff357
heailer <132157560+heailer@users.noreply.github.com>
2025-01-01 14:57:45 +0530

    Update App.java

--- src/main/java/com/app/demo/javaMavenExample/App.java
+++ src/main/java/com/app/demo/javaMavenExample/App.java
@@ -6,5 +6,6 @@ package com.app.demo.javaMavenExample;
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("This is your first java maven project!");
     }
 }
